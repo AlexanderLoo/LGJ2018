2656b7170e5dbb3416eb55fa93d2a930631fc0e4
Sebastian Rodriguez (Escombruara) <seb_110@hotmail.com>
2018-01-26 21:48:41 -0500

    Codigo
    Codigo

---
+++ Wassup/Assets/Scripts/Chat.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Chat : MonoBehaviour {
+
+	public InputField cuadroDeTexto;
+	public string texto;
+	private string nuevoTexto;
+	public int indice;
+
+	char[] letras;
+	// Use this for initialization
+	void Start () {
+		indice = 0;
+		letras  = texto.ToCharArray();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		 
+		if (Input.GetKeyDown (KeyCode.Space)){
+			Verificar (KeyCode.Space);
+			return;
+		}
+
+		if (Input.GetKeyDown (KeyCode.Backspace)){
+			cuadroDeTexto.text = "";
+			indice = 0;
+			return;
+
+		}
+
+		if (Input.anyKeyDown)
+			Verificar(KeyCode.Print);
+		
+
+	}
+
+
+	public void Verificar (KeyCode _tecla){
+		
+		if (indice < letras.Length){
+
+			if (letras[indice] == ' '){
+				if (_tecla.ToString () != "Space")
+					return;    
+			}
+
+			cuadroDeTexto.text += letras[indice];
+			indice++;  
+		}
+	}
+}

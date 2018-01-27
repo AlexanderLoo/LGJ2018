85b7b39265f02410fe84d842ff4834f8fe4325b3
Luis Pineda <luis.alonso97@outlook.com>
2018-01-27 02:59:41 -0500

    Modificacion en las animaciones iniciales

--- Wassup/Assets/Scripts/Scripts Luiz/ManagerTest.cs
+++ Wassup/Assets/Scripts/Scripts Luiz/ManagerTest.cs
@@ -7,10 +7,20 @@ public class ManagerTest : MonoBehaviour
 {
 
     public Image TestImage;
+
     public float animSpeed;
+    public float colorSpeed;
+    public float moveSpeed;
+
     private Vector3 bigSize;
     private Vector3 midSize;
     private Vector3 smallSize;
+
+    private Vector2 initialPos;
+    private Vector2 finalPos;
+
+    private Color initialColor;
+    private Color finalColor;
 
 
     // Use this for initialization
@@ -19,7 +29,13 @@ public class ManagerTest : MonoBehaviour
 
         bigSize = new Vector3(1, 1, 1);
         midSize = new Vector3(.6f, .6f, .6f);
-        smallSize = new Vector3(.1f, .1f, .1f);
+        smallSize = new Vector3(.1f, .062f, .1f);
+
+        initialColor = new Color(255, 0, 0, 255);
+        finalColor = new Color(TestImage.color.r, TestImage.color.g, TestImage.color.b, 0);
+
+        initialPos = TestImage.rectTransform.anchoredPosition;
+        finalPos = new Vector3(-213, TestImage.rectTransform.anchoredPosition.y);
 
     }
 
@@ -27,42 +43,97 @@ public class ManagerTest : MonoBehaviour
     void Update()
     {
 
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.S))
         {
-            // Debug.Log("lol");
-            StartCoroutine(ShrinkImage());
+            StartCoroutine(ScaleDownImage());
         }
 
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            StartCoroutine(ScaleUpImage());
+        }
 
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            StartCoroutine(MoveLeftImage());
+        }
+
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            StartCoroutine(MoveRight());
+        }
     }
 
-    IEnumerator ShrinkImage()
+    IEnumerator ScaleDownImage()
     {
         // Debug.Log("put");
         float t = 0;
         while (t < 1)
         {
             t += Time.deltaTime * animSpeed;
-            TestImage.rectTransform.localScale = Vector3.Lerp(bigSize, midSize, t);
+            TestImage.rectTransform.localScale = Vector3.Lerp(bigSize, smallSize, t);
 
             yield return new WaitForEndOfFrame();
         }
 
-        StartCoroutine(FadeOutImage());
+        // StartCoroutine(FadeOutImage());
     }
 
-    IEnumerator FadeOutImage()
+    IEnumerator ScaleUpImage()
     {
         float t = 0;
         while (t < 1)
         {
             t += Time.deltaTime * animSpeed;
-            TestImage.rectTransform.localScale = Vector3.Lerp(midSize, smallSize, t);
+            TestImage.rectTransform.localScale = Vector3.Lerp(smallSize, bigSize, t);
 
             yield return new WaitForEndOfFrame();
         }
-
     }
+
+    IEnumerator MoveLeftImage()
+    {
+        float t = 0;
+        while (t < 1)
+        {
+            t += Time.deltaTime * moveSpeed;
+            TestImage.rectTransform.anchoredPosition = Vector2.Lerp(initialPos, finalPos, t);
+
+            yield return new WaitForEndOfFrame();
+
+
+        }
+    }
+
+    IEnumerator MoveRight()
+    {
+        float t = 0;
+        while (t < 1)
+        {
+            t += Time.deltaTime * moveSpeed;
+            TestImage.rectTransform.anchoredPosition = Vector2.Lerp(finalPos, initialPos, t);
+
+            yield return new WaitForEndOfFrame();
+
+
+        }
+    }
+
+    // IEnumerator FadeOutImage()
+    // {
+    //     float t = 0;
+    //     // float z = 0;
+    //     while (t < 1)
+    //     {
+    //         t += Time.deltaTime * animSpeed;
+    //         // z += 0.1f;
+    //         // TestImage.color = Color.Lerp(initialColor, finalColor, t);
+    //         TestImage.rectTransform.localScale = Vector3.Lerp(midSize, smallSize, t);
+
+    //         yield return new WaitForEndOfFrame();
+    //     }
+
+    // }
 
 
 }

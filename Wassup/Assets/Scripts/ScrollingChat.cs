600e79a919dcf0f0c779fcaf4fca63f2fc98cc5e
AlexanderLoo <AlexanderLoo>
2018-01-27 01:25:36 -0500

    Basic Chat Scroll

---
+++ Wassup/Assets/Scripts/ScrollingChat.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScrollingChat : MonoBehaviour {
+
+	private RectTransform _transform;
+	public float maxInY = 2000;
+
+	void Start(){
+
+		_transform = GetComponent<RectTransform> ();
+	}
+
+	void Update(){
+
+		_transform.Translate (Vector3.up * 10);
+		Reposition ();
+	}
+
+	void Reposition(){
+
+		if (_transform.position.y >= maxInY) {
+			Vector3 newPosition = _transform.position;
+			newPosition.y -= _transform.sizeDelta.y * 2;
+			_transform.position = newPosition;
+		}
+	}
+}

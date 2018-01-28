1aff2e7e7d304c5dc89655ef2fb2205e36bd4700
AlexanderLoo <AlexanderLoo>
2018-01-28 07:51:00 -0500

    AnimationScript

---
+++ Wassup/Assets/Scripts/GilbertoAnimation.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GilbertoAnimation : MonoBehaviour {
+
+	private Image image;
+
+	public Image[] idleImages;
+	public Image[] actionImages;
+	//velocidad en segundos
+	public float speed = 0.5f;
+	public bool action;
+	private float timer;
+	private int index;
+
+	void Start(){
+
+		timer = speed;
+	}
+
+	void Update(){
+		if (action) {
+			AnimationManager (actionImages);
+		} else {
+			AnimationManager (idleImages);
+		}
+	}
+
+	void AnimationManager(Image[]imageList){
+
+		timer -= Time.deltaTime;
+		if (timer <= 0) {
+			timer = speed;
+			image = imageList [index];
+			if (index < imageList.Length - 2) {
+				index++;
+			} else {
+				index = 0;
+			}
+		}
+	}
+
+}

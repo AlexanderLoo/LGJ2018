b2bbf9ad4595924457b87e02e834c8f8a215d503
AlexanderLoo <AlexanderLoo>
2018-01-28 12:52:33 -0500

    Last Scene Finish

---
+++ Wassup/Assets/Scripts/FinalScene.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FinalScene : MonoBehaviour {
+
+	private AudioSource _as;
+
+	public GameObject credits;
+
+	void Start(){
+
+		_as = GetComponent<AudioSource> ();
+	}
+
+	public void PlayMusic(){
+
+		_as.Play ();
+	}
+
+	public void ShowCredits(){
+
+		credits.SetActive (true);
+	}
+}

e0394a9781a724d1743a0d7eddd9ce4fe3603d21
AlexanderLoo <AlexanderLoo>
2018-01-28 08:23:50 -0500

    GilbertoAnimation

--- Wassup/Assets/Scripts/GilbertoAnimation.cs
+++ Wassup/Assets/Scripts/GilbertoAnimation.cs
@@ -7,20 +7,25 @@ public class GilbertoAnimation : MonoBehaviour {
 
 	private Image image;
 
-	public Image[] idleImages;
-	public Image[] actionImages;
+	public Sprite[] idleImages;
+	public Sprite[] actionImages;
 	//velocidad en segundos
 	public float speed = 0.5f;
 	public bool action;
 	private float timer;
 	private int index;
 
+	void Awake(){
+		image = GetComponent<Image> ();
+	}
 	void Start(){
 
 		timer = speed;
+		StartCoroutine (Flipping ());
 	}
 
 	void Update(){
+		Flipping ();
 		if (action) {
 			AnimationManager (actionImages);
 		} else {
@@ -28,18 +33,35 @@ public class GilbertoAnimation : MonoBehaviour {
 		}
 	}
 
-	void AnimationManager(Image[]imageList){
+	void AnimationManager(Sprite[]spriteList){
 
 		timer -= Time.deltaTime;
 		if (timer <= 0) {
 			timer = speed;
-			image = imageList [index];
-			if (index < imageList.Length - 2) {
+			image.sprite = spriteList [index];
+			if (index < spriteList.Length - 1) {
 				index++;
 			} else {
 				index = 0;
 			}
 		}
+	}
+
+	IEnumerator Flipping(){
+
+		while (true) {
+			yield return new WaitForSeconds (3);
+			RandomScale ();
+		}
+	}
+	//Función para flipear el personaje
+	void RandomScale(){
+
+		int[] i = { -1, 1 };
+		int random = Random.Range (0, 2);
+		Vector3 newScale = transform.localScale;
+		newScale.x = i [random];
+		transform.localScale = newScale;
 	}
 
 }

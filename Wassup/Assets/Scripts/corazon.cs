ffd1d30c2d8987b45838472d99541b648e7504d7
Rodrigo Sifuentes <sifuentesrodrigo@gmail.com>
2018-01-28 13:00:53 -0500

    victory scene

---
+++ Wassup/Assets/Scripts/corazon.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class corazon : MonoBehaviour {
+
+	public Transform heart;
+	public bool agrandar;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+	
+	// Update is called once per frame
+	void Update () {
+
+
+		if (heart.localScale.x < 1.0f) {
+			agrandar = true;
+		}
+		if (heart.localScale.x > 1.5f) {
+			agrandar = false;
+		}
+		if (agrandar == true) {
+				heart.localScale += new Vector3 (0.01F, 0.01F, 0);
+			}
+
+		if (agrandar == false) {
+			heart.localScale -= new Vector3 (0.01F, 0.01F, 0);
+		}
+	}
+
+}
